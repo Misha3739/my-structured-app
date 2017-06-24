1678a997ffa09be55ed54cee385635a6b12caa6d
M.Udot <mihail.udot@yandex.ru>
2017-06-24 12:37:37 +0300

    More components implemented

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,9 +10,13 @@ export class RecipesListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This the meat',
-'http://www.seriouseats.com/images/2017/02/20170228-pressure-cooker-recipes-roundup-collage.jpg' )
+      'A Meat Recipe',
+      'This is the meat',
+'http://www.seriouseats.com/images/2017/02/20170228-pressure-cooker-recipes-roundup-collage.jpg' ),
+    new Recipe(
+      'A Salad Recipe',
+      'This is the salad',
+      'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg' )
   ];
 
   constructor() { }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('egg', 5),
+    new Ingredient('apple', 3)
+  ];
 
   constructor() { }
 
