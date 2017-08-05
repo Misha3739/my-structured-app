e367697e01b96af24c2504410351f571987c59dc
Misha3739 <mihail.udot@yandex.ru>
2017-08-05 23:11:55 +0300

    Routing added. Start routing

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch:'full'},
+  {path: 'recipes',component: RecipesComponent},
+  {path:'shopping-list', component: ShoppingListComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
