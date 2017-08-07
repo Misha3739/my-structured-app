c5cf2e3b3aa22b737e82434e3e3ab35767754173
Misha3739 <mihail.udot@yandex.ru>
2017-08-07 14:52:19 +0300

    Navigation for recipes in UI with routing fixed

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -11,7 +11,7 @@ import {RecipeService} from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
 
 @Input('recipe') recipe: Recipe;
-
+@Input() id: number;
   ngOnInit() {
   }
 
