aab47f366d89c9cb1a542aacfeb4b465f06238e3
Misha3739 <mihail.udot@yandex.ru>
2017-08-14 17:53:04 +0300

    Cosmetic fixes; Unsubscibe umplemented

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {RecipeService} from "../recipe.service";
 import {Recipe} from "../recipe.model";
+import {Subscription} from "rxjs/Subscription";
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
 
+  subscription: Subscription;
+
   constructor(private route: ActivatedRoute, private  recipeService: RecipeService, private  router: Router) { }
 
   ngOnInit() {
-    this.route.params.subscribe(
+   this.subscription =  this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
 
@@ -88,7 +91,7 @@ export class RecipeEditComponent implements OnInit {
       }));
   }
 
-  onDelete(id: number)
+  onDeleteIgredient(id: number)
   {
     (<FormArray>this.recipeForm.get('ingredients')).removeAt(id);
   }
@@ -96,5 +99,10 @@ export class RecipeEditComponent implements OnInit {
   onCancel()
   {
     this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy()
+  {
+    this.subscription.unsubscribe();
   }
 }
