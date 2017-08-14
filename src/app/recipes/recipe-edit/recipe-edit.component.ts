a7209fabdb80c9d6769ad208790a1cbe00651d4f
Misha3739 <mihail.udot@yandex.ru>
2017-08-14 12:10:40 +0300

    Ingredients list synchronized for form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
-
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
@@ -9,8 +10,9 @@ import {ActivatedRoute, Params} from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private  recipeStrvice: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -18,7 +20,50 @@ export class RecipeEditComponent implements OnInit {
         this.id = +params['id'];
 
         this.editMode = params['id'] != null ? true : false;
+        this.initForm();
     });
   }
 
+  initForm()
+  {
+    let recipeName = '', recipeImagePath = '', recipeDescription='';
+    let recipeIngredients = new FormArray([]);
+    if(this.editMode)
+    {
+      const recipe =  this.recipeStrvice.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+
+
+      if(recipe['ingredients'])
+      {
+        for(let ingredient of recipe.ingredients)
+        {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          }));
+        }
+      }
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
+
+    });
+  }
+
+  onSubmit()
+  {
+    console.log(this.recipeForm);
+  }
+
+  onAddIngredient()
+  {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.recipeForm.get('ingredients')).push(control);
+  }
 }
