6d7961dfb8d87caf2e3658fdddefb0b7396cd47d
Misha3739 <mihail.udot@yandex.ru>
2017-08-20 19:05:44 +0300

    FetchRecipesOnAppLoading

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit {
   constructor(private  storageService: DataStorage) { }
 
   ngOnInit() {
+    this.storageService.getRecipes();
   }
 
   onSaveData()
